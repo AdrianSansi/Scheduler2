d859dd3eef90d594627e1de08d3c33ec8ed8e0ba
Adrián Sansiñena Rodríguez <adriansansi@gmail.com>
2022-06-15 12:15:23 +0200

    Duplicados en next hour, next minute y next second corregidos a priori

---
+++ Scheduler2/NextClasses/NextDayCheck.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler2
+{
+    public class NextDayCheck
+    {
+        public static DateTime checkIfMustBeNextDay(DateTime current, DateTime auxiliar, Settings settings)
+        {
+            int day = current.Day;
+            if (auxiliar.Day != day || auxiliar.TimeOfDay > settings.EndTime.TimeOfDay)
+            {
+                switch (settings.Format)
+                {
+                    case Format.Weekly:
+                        if (NextDay.WeeklyFormat(settings) == 0)
+                        {
+                            return current;
+                        }
+                        current = SetTimeDate(current, settings);
+                        return current.AddDays(NextDay.WeeklyFormat(settings));
+                    case Format.Monthy:
+                        current = SetTimeDate(current, settings);
+                        return NextDay.MonthyFormat(settings);
+                    default:
+                        current = SetTimeDate(current, settings);
+                        return NextDay.DailyFormat(current, settings);
+
+                }
+            }
+            else
+            {
+                return auxiliar;
+            }
+
+
+        }
+
+        private static DateTime SetTimeDate(DateTime current, Settings settings)
+        {
+            current -= current.TimeOfDay;
+            current += settings.StartTime.TimeOfDay;
+            return current;
+        }
+    }
+}
--- Scheduler2/NextClasses/NextSecond.cs
+++ Scheduler2/NextClasses/NextSecond.cs
@@ -3,42 +3,10 @@ namespace Scheduler2
 {
     internal class NextSecond
     {
-
-        private static DateTime SetTimeDate(DateTime current, Settings settings)
-        {
-            current -= current.TimeOfDay;
-            current += settings.StartTime.TimeOfDay;
-            return current;
-        }
-
         public static DateTime Calculate(DateTime current, Settings settings)
         {
-            int day = current.Day;
             DateTime auxiliar = current.AddSeconds(settings.TimePeriod);
-            if (auxiliar.Day != day || auxiliar.TimeOfDay > settings.EndTime.TimeOfDay)
-            {
-                switch (settings.Format)
-                {
-                    case Format.Weekly:
-                        if (NextDay.WeeklyFormat(settings) == 0)
-                        {
-                            return current;
-                        }
-                        current = SetTimeDate(current, settings);
-                        return current.AddDays(NextDay.WeeklyFormat(settings));
-                    case Format.Monthy:
-                        current = SetTimeDate(current, settings);
-                        return NextDay.MonthyFormat(settings);
-                    default:
-                        current = SetTimeDate(current, settings);
-                        return NextDay.DailyFormat(current, settings);
-
-                }
-            }
-            else
-            {
-                return auxiliar;
-            }
+            return NextDayCheck.checkIfMustBeNextDay(current, auxiliar, settings);
         }
         
     }
