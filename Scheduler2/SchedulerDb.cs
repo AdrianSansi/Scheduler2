ddfe1449299f6b917f7ae189e6bcd0bd126f2b03
Adrián Sansiñena Rodríguez <adriansansi@gmail.com>
2022-06-28 14:01:39 +0200

    Non-nullable warning evitado

--- Scheduler2/SchedulerDb.cs
+++ Scheduler2/SchedulerDb.cs
@@ -6,8 +6,9 @@ namespace Scheduler2
 {
     public class SchedulerDb : DbContext
     {
-        public virtual DbSet<Settings>? Settings { get; set; }
-              
-       
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public virtual DbSet<Settings> Settings { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
     }
 }
